d5a5165d7351baa770ee5ee51b2c09cbd698f53f
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-08-10 18:36:09 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./components/Root/Root";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from './components/Register/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
